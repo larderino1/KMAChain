90ed424d9961153b2bcc0108c35a6c27cc33cc3f
denis <losangelesdenis2002@gmail.com>
2021-03-27 10:04:36 +0200

    Final

---
+++ model.js
@@ -0,0 +1,53 @@
+const DataModel = {
+    "denys.denys@ukma.edu.ua": {
+        "fullName": "Denys Denys Denys",
+        "faculty": "FI",
+        "specialty": "Computer Science",
+        "year": 2,
+        "semesters": [
+            {
+                "semester": 1,
+                "subjects": [
+                    {
+                        "name": "Blockchain and cryptocurrencies",
+                        "lector": "lector@ukma.edu.ua",
+                        "mark": 80,
+                        "topics": [
+                            {
+                                "title": "Bitcoin",
+                                "mark": 25
+                            },
+                            {
+                                "title": "Hyperledger Fabric",
+                                "mark": 25
+                            },
+                            {
+                                "title": "Ethereum",
+                                "mark": 30
+                            }
+                        ]
+                    },
+                    {
+                        "name": "Computer Science",
+                        "lector": "lector@ukma.edu.ua",
+                        "mark": 99,
+                        "topics": [
+                            {
+                                "title": "Java",
+                                "mark": 33
+                            },
+                            {
+                                "title": "C#",
+                                "mark": 33
+                            },
+                            {
+                                "title": "C++",
+                                "mark": 33
+                            },
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
